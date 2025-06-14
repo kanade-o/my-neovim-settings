7e2cdcdbad992199ef185aa766e5d146b4596463
kanade-o <sotaohnuma@gmail.com>
2025-06-14 22:41:27 +0900

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,22 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("i", "jk", "<Esc>", opts)
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
